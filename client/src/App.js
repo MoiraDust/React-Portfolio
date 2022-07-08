55b4eb890d2591ad5f09220f61e317acec4c6df0
MoiraDust <moiradust@foxmail.com>
2022-07-09 00:29:50 +1000

    Profile

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./PortfolioContainer/Home/Home";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Typical from "react-typical";
+import "./Profile.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="#">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="#">
+                <i className="fa fa-github-square"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Moira</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Developer 😼",
+                    1000,
+                    "Front End Developer 👩‍💻",
+                    1000,
+                    "MERN Developer 🥸",
+                    1000,
+                    "Backend Developer 💻",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-details-tagline">
+                {" "}
+                You can download my CV if you are intrested.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">Hire Me</button>
+            <a href="#resumeNameHere" download="Yi_Yang_(Moira).pdf">
+              <button className="btn highlighted-btn">Download My CV</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
