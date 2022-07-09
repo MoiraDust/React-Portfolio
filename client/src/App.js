d3735e8fe6516774cddf6ad1ca38c0a009dd4e9d
MoiraDust <moiradust@foxmail.com>
2022-07-10 02:10:26 +1000

    Observable

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Home from "./PortfolioContainer/Home/Home";
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+// import Home from "./PortfolioContainer/Home/Home";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <PortfolioContainer />
       </div>
     );
   }
