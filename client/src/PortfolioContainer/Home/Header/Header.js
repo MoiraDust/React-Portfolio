caac1eae1ce161fd7d1587d3baeb37a2dc46cb35
MoiraDust <moiradust@foxmail.com>
2022-07-11 02:21:48 +1000

    responsive header

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -19,5 +19,63 @@ export default function Header() {
   };
   let currentScreenSubscription =
     ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
-  return <div>Header</div>;
+
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  //   mark selected options
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option";
+    if (index < TOTAL_SCREENS.length - 1) classes += " header-option-seperator";
+    if (selectedScreen === index) classes += "selected-header-option";
+    return;
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
+
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+
+  return (
+    <div>
+      <div
+        className="header-container"
+        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+      >
+        <div className="header-parent">
+          <div
+            className="header-hamburger"
+            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+          >
+            <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+          </div>
+          <div className="header-logo">
+            <span>MOIRA</span>
+          </div>
+          <div
+            className={
+              showHeaderOptions
+                ? "header-options show-hamburger-options"
+                : "header-options"
+            }
+          >
+            {getHeaderOptions()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
